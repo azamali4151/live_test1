41569741f3f80be15d54dee271bdd1252578a1c8
Md.Azam Ali <azam.ali@nationalpolymer.net>
2024-03-15 23:50:07 +0600

    update

--- bin/live_test.dart
+++ bin/live_test.dart
@@ -1,3 +1,27 @@
 main(){
-  print('dart');
+  List<Map> fruits = [
+    {'name': 'Apple', 'color': 'Red', 'price': 2.5},
+    {'name': 'Banana', 'color': 'Yellow', 'price': 1.0},
+    {'name': 'Grapes', 'color': 'Purple', 'price': 3.0},
+  ];
+
+  displayFruitDetails(fruits);
+
+  double disPercentage = 10;
+  applyPriceDiscount(fruits, disPercentage);
+
+  print('After set discount');
+  displayFruitDetails(fruits);
+}
+
+void displayFruitDetails(List<Map> fruits) {
+  fruits.forEach((fruit) => print('Name: ${fruit['name']}, Color: ${fruit['color']}, Price: \$${fruit['price']}'));
+}
+
+void applyPriceDiscount(List<Map> fruits, double disPercentage) {
+  for (var fruit in fruits) {
+    double currentPrice = fruit['price'];
+    double discountedPrice = currentPrice * (1 - disPercentage / 100);
+    fruit['price'] = discountedPrice;
+  }
 }
